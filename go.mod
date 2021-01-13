8f70913f4935492ca41ead747f27ae73a77828e1
ahall326 <angelarhall@gmail.com>
2021-01-12 22:34:41 -0700

    commit to master

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ahall26/storj
+
+go 1.15
+
+require (
+	github.com/spf13/cobra v1.1.1
+	github.com/zeebo/errs v1.2.2
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad
+	gopkg.in/spacemonkeygo/monkit.v2 v2.0.0-20190623001553-09813957f0a8
+	storj.io/private v0.0.0-20210108233641-2ba1ef686d1f
+)
---
+++ sendalert.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+func SendAlert(ciphertext string) {
+
+	url := "https://api.pagerduty.com/incidents"
+	method := "POST"
+
+	payload := strings.NewReader("{\n    \"incident\": {\n        \"type\": \"incident\",\n        \"title\": \"Decryption Error\",\n        \"service\": {\n            \"id\": \"PEIVHZD\",\n            \"type\": \"service_reference\"\n        },\n        \"body\": {\n            \"type\": \"incident_body\",\n            \"details\": \" Failed Cipher Text: " + ciphertext + " \"\n        }\n    }\n}")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	req.Header.Add("Authorization", "Token token =aETAE2ajHFYssfyXpnZR")
+	req.Header.Add("Accept", "application/vnd.pagerduty+json;version=2")
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("From", "Regulus-Ain-efc7b0b0@dispostable.com")
+
+	res, err := client.Do(req)
+	defer res.Body.Close()
+
+}
